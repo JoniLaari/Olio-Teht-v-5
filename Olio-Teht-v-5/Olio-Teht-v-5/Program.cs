5c2c26f27180f652bf37674d10ac7ef441c26648
h8324 <h8324@MEDI-426-WS-20>
2017-01-24 20:14:00 +0200

    Initial  commit.

---
+++ Olio-Teht-v-5/Olio-Teht-v-5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_Teht_v_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sekunti;
+            Console.Write("Anna sekunnit > ");
+            sekunti = int.Parse(Console.ReadLine());
+            int minuutti = sekunti % 60;
+            int tunti = sekunti % 3600;
+            Console.WriteLine("Antamasi sekuntiaika " tunti "tunti (a)", minuutti, "minuuttia");
+            Console.WriteLine();
+        }
+    }
+}
